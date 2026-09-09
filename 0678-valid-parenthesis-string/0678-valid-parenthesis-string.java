488875f9c5bd38f6c9bda59f2b30e90afd9afa3b
Abhibhavya Gupta <abhibhavya.gupta2005@gmail.com>
2026-09-10 00:34:52 +0530

    Time: 1 ms (27.5%), Space: 43.9 MB (6.43%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,47 @@
+class Solution {
+    int n;
+    String str;
+    Boolean[][] dp;
+    public boolean checkValidString(String s) {
+       n=s.length();
+       dp = new Boolean[n+1][n];
+       str=s;
+       return backtrack(0,0);
+
+    }
+    boolean backtrack(int i, int bal)
+    {
+        if(bal<0) 
+       {
+        return false;
+       }
+        
+        if(i==n) 
+        {
+            return bal==0;
+        }
+       
+        if(dp[i][bal]!=null)
+        {
+            return dp[i][bal];
+        }
+
+
+         if(str.charAt(i)=='(')
+         {
+          dp[i][bal] = backtrack(i+1,bal+1);
+         }
+          else if(str.charAt(i)==')')
+          {
+          dp[i][bal] = backtrack(i+1,bal-1);
+         }
+         else{
+            dp[i][bal]=backtrack(i+1,bal+1) 
+            || backtrack(i+1,bal-1)
+            || backtrack(i+1,bal);
+         }
+          
+          return dp[i][bal];
+
+    }
+}
